56253629b3ca270c38081fb57ecbb0f5e4417c2c
Amit Sharma <asharma7001@gmail.com>
2025-01-05 20:48:27 +0530

    signup and login and customer controller

--- src/main/java/com/aks/jpaExample/controller/CustomerController.java
+++ src/main/java/com/aks/jpaExample/controller/CustomerController.java
@@ -1,9 +1,6 @@
 package com.aks.jpaExample.controller;
 
 import com.aks.jpaExample.entity.CustomerDto;
-import com.aks.jpaExample.model.Login;
-import com.aks.jpaExample.model.SignUp;
-import com.aks.jpaExample.service.AuthService;
 import com.aks.jpaExample.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -11,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-public class CusotmerController {
+public class CustomerController {
     @Autowired
     CustomerService service;
 
